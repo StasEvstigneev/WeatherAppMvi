52d353197cd5ccc8bf57cd39a12746166e9eda46
Stas <137781307+StasEvstigneev@users.noreply.github.com>
2025-06-27 10:15:02 +0300

    Search Content

--- app/src/main/java/com/esy/weatherappmvi/presentation/search/SearchContent.kt
+++ app/src/main/java/com/esy/weatherappmvi/presentation/search/SearchContent.kt
@@ -1,8 +1,176 @@
 package com.esy.weatherappmvi.presentation.search
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.esy.weatherappmvi.R
+import com.esy.weatherappmvi.domain.model.City
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchContent(component: SearchComponent) {
 
+    val state by component.model.collectAsState()
+
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(key1 = Unit) {
+        focusRequester.requestFocus()
+    }
+
+    SearchBar(
+        inputField = {
+            SearchBarDefaults.InputField(
+                query = state.searchQuery,
+                onQueryChange = { component.changeSearchQuery(query = it) },
+                onSearch = { component.onClickSearch() },
+                expanded = true,
+                onExpandedChange = { TODO() },
+                placeholder = { Text(text = stringResource(R.string.search)) },
+                leadingIcon = {
+                    IconButton(onClick = { component.clickBack() }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = null
+                        )
+                    }
+                },
+                trailingIcon = {
+                    IconButton(onClick = { component.onClickSearch() }) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = null
+                        )
+                    }
+                },
+                colors = TODO(),
+                interactionSource = TODO()
+            )
+        },
+        modifier = Modifier.focusRequester(focusRequester),
+        expanded = true,
+        onExpandedChange = { }
+    ) {
+        when (val searchState = state.searchState) {
+            SearchStore.State.SearchState.EmptyResult -> {
+                Text(
+                    text = stringResource(R.string.nothing_found),
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+
+            SearchStore.State.SearchState.Error -> {
+                Text(
+                    text = stringResource(R.string.error),
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+
+            SearchStore.State.SearchState.Initial -> {}
+            SearchStore.State.SearchState.Loading -> {
+                Loading()
+            }
+
+            is SearchStore.State.SearchState.SearchResults -> {
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    contentPadding = PaddingValues(16.dp)
+                ) {
+                    items(
+                        items = searchState.cities,
+                        key = { it.id }
+                    ) {
+                        CityCard(
+                            city = it,
+                            onCityClick = { component.onClickCity(it) }
+                        )
+
+                    }
+
+                }
+
+            }
+        }
+
+    }
+
+}
+
+
+@Composable
+private fun CityCard(
+    city: City,
+    onCityClick: () -> Unit
+) {
+    Card(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { onCityClick() }
+                .padding(
+                    vertical = 8.dp,
+                    horizontal = 16.dp
+                )
+        ) {
+            Text(
+                text = city.name,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = city.country,
+                style = MaterialTheme.typography.titleMedium
+            )
+
+        }
+    }
+
+}
+
+@Composable
+private fun Loading() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.align(Alignment.Center),
+            color = Color.White
+        )
+    }
 }
